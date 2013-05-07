edee32dfc2b92af70e67060018752312d239c38f
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2013-05-07 09:36:53 +0200

    Imported scripts for 1.2

--- scripts/JobDBAdminPurger.sh
+++ scripts/JobDBAdminPurger.sh
@@ -32,7 +32,7 @@ done
 classPath=/usr/share/java/glite-ce-cream-clients.jar:/usr/share/java/glite-ce-common-java.jar:/usr/share/java/glite-ce-cream-api-java.jar:/usr/share/java/glite-ce-cream-blahExecutor.jar:/usr/share/java/glite-ce-cream-core.jar:/usr/share/java/argus-pep-api-java.jar:/usr/share/java/argus-pep-common.jar:/usr/share/java/voms-api-java.jar:/usr/share/java/jakarta-commons-httpclient.jar:/usr/share/java/mysql-connector-java.jar:/usr/share/java/servlet.jar
 
 if [ $tomcat_version == "tomcat6" ];then
-   classPath_Tomcat=$CATALINA_HOME/lib/commons-dbcp.jar:$CATALINA_HOME/lib/commons-pool.jar:$CATALINA_HOME/lib/commons-logging.jar:$CATALINA_HOME/lib/log4j.jar:$CATALINA_HOME/lib/[bcprov].jar:$CATALINA_HOME/lib/[trustmanager].jar
+   classPath_Tomcat=/usr/share/java/commons-logging.jar:$CATALINA_HOME/lib/commons-dbcp.jar:$CATALINA_HOME/lib/commons-pool.jar:$CATALINA_HOME/lib/log4j.jar:$CATALINA_HOME/lib/[bcprov].jar:$CATALINA_HOME/lib/[trustmanager].jar
 else
    classPath_Tomcat=$CATALINA_HOME/common/lib/[commons-dbcp].jar:$CATALINA_HOME/common/lib/[commons-pool].jar:$CATALINA_HOME/server/lib/[commons-logging].jar:$CATALINA_HOME/server/lib/[log4j].jar:$CATALINA_HOME/server/lib/[bcprov].jar:$CATALINA_HOME/server/lib/[trustmanager].jar
 fi
